14512014b7ad52ca5f13fe8cbdb210ceb9fd5f7b
Elstinko <42773949+Elstinko@users.noreply.github.com>
2019-06-17 14:00:16 -0600

    Update FFX_Dodger.ino

--- FFX_Dodger/FFX_Dodger.ino
+++ FFX_Dodger/FFX_Dodger.ino
@@ -8,6 +8,7 @@ Servo servo1;
 const int lightPin = A0;     // LDR Pin
 const int servoPin = 9;      // Servo Pin 
 int angle = 0;               // Servo Angle
+int servoMove = 70;          // The amount the servo moves in degrees
 int count = 0;               // Counts number of dodges
 
 // Begin serial communication with a computer if one is plugged in via USB
@@ -26,20 +27,20 @@ Serial.println(analogRead(lightPin));
 delay(200);
 
 // Checks to see if lightning has flashed 
-// The "400" is the number you will need to change that will trigegr the servo to dodge in the game
+// The "400" is the number you will need to change that will trigger the servo to dodge in the game
 if(analogRead(lightPin) >= 400){
   // The next two lines provide a counter of the number of lightning bolts currently dodged if you have the Arduino plugged into a PC
   count++;
   Serial.println(count);
 
   // Moves servo 70 degrees to press x button
-  for (angle = 0; angle <= 70; angle += 10) {
+  for (angle = 0; angle <= servoMove; angle += 10) {
     servo1.write(angle);
     delay(30);  
   }
 
   // Moves servo 70 degrees back to its original position
-  for (angle = 70; angle <= 0; angle -= 10) {
+  for (angle = servoMove; angle <= 0; angle -= 10) {
     servo1.write(angle);
     delay(30);  
   }
